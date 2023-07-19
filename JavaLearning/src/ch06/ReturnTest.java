bee4e241f88c2639ee2374acb65fd5ca530f11a8
Alex-MJY <jmoon0227@gmail.com>
2023-07-19 15:04:38 +0900

    java OOP 3

---
+++ JavaLearning/src/ch06/ReturnTest.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class ReturenTest {
+    public static void main(String[] args) {
+        
+    }
+    int result1(int a, int b) {
+        return a + b;
+    }
+
+    void result2(int a, int b, int[] result) {
+        result[0] = a + b;
+    }
+
+}
