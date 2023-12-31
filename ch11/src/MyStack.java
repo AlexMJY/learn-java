63185b6aa4d2f9e15f208db54112f9bdf1c82541
Alex-MJY <jmoon0227@gmail.com>
2023-12-31 14:27:51 +0900

    java ch11 Stack, Queue

---
+++ ch11/src/MyStack.java
@@ -0,0 +1,25 @@
+//import java.util.*;
+//
+//public class MyStack {
+//    public Object push(Object item) {
+//        addElement(item);
+//        return item;
+//    }
+//
+//    public Object pop() {
+//        removeElmentAt(size() - 1);
+//        return obj;
+//    }
+//
+//    public boolean empty() {
+//        return size() == 0;
+//    }
+//
+//    public int search(Object o) {
+//        int i = lastIndexOf(o);
+//        if (i >= 0) {
+//            return size() - i;
+//        }
+//        return -1;
+//    }
+//}
